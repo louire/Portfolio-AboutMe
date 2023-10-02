c9d2ef67d38fa41646b21c787474f61137efd9b5
Loui Recio <lreciopersonal@gmail.com>
2023-10-02 17:23:15 -0500

    SkeletonInit-First

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">My name is Loui Recio Izaguirre</h2>
+		
+		<div class="flex justify-center space-x-2">
+			
+		</div>
+		<div class="space-y-2">
+			<p class="text-lg">
+				I'm a Software Engineer.
+			</p>
+			<p class="text-lg">
+				I'm also a student at the Institute Of Technology of Matamoros,
+				taking up BS Computer Systems Engineering.
+			</p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	
+</style>
